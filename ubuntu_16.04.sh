0bd473396d98564f9b43298ca14dd19e95852f86
mykola <lavirozp@gmail.com>
2016-06-10 16:46:28 +0300

    init

---
+++ ubuntu_16.04.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y \
+    mc \
+    htop \
+    curl \
+    git \
+
+
+# nginx
+apt-get install -y nginx
+
+#php-fpm
+apt-get install -y php-fpm \
+    php-curl \
+    php-zip \
+    php-gd \
+    php-mbstring \
+    php-mcrypt \
+    php-opcache \
+    php-intl \
+    php-mysql \
+
+service php7.0-fpm restart
+
+#mysql
+apt-get install -y mysql-server
+
+
+
+#Composer
+curl -sS https://getcomposer.org/installer | php \
+    && mv composer.phar /usr/local/bin/composer \
+    && chmod 755 /usr/local/bin/composer
+
+composer global require "fxp/composer-asset-plugin:~1.1.1"
