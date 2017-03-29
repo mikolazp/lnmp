78b27b708cab2d2eece08e28472b96aa598e6882
mykola <lavirozp@gmail.com>
2017-03-29 22:32:00 +0300

    -

--- ubuntu_16.04.sh
+++ ubuntu_16.04.sh
@@ -38,4 +38,4 @@ curl -sS https://getcomposer.org/installer | php \
     && mv composer.phar /usr/local/bin/composer \
     && chmod 755 /usr/local/bin/composer
 
-composer global require "fxp/composer-asset-plugin:~1.1.1"
+composer global require "fxp/composer-asset-plugin:^1.2.0"
