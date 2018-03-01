0b8caa675cd0476d2156ea1f97b30dfd5f2a9f54
mykola <lavirozp@gmail.com>
2018-03-01 14:08:53 +0200

    remove fxp/composer-asset-plugin

--- ubuntu_16.04.sh
+++ ubuntu_16.04.sh
@@ -48,4 +48,3 @@ curl -sS https://getcomposer.org/installer | php \
     && mv composer.phar /usr/local/bin/composer \
     && chmod 755 /usr/local/bin/composer
 
-composer global require "fxp/composer-asset-plugin:^1.2.0"
